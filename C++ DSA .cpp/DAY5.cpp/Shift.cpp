d5a4b84e92ae46315f9de0dbba3a0bc33398d8c0
RISHI  KUMAR  THAKUR <119896306+RISHIKUMARTHAKUR@users.noreply.github.com>
2023-06-01 11:41:53 +0530

    Bitwise operator: Right shift and Left shift

---
+++ C++ DSA .cpp/DAY5.cpp/Shift.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<(17>>1)<<endl;
+
+    cout<<(17<<1)<<endl;
+
+    cout<<(17>>2)<<endl;
+
+    cout<<(17<<2)<<endl;
+
+    cout<<(25>>1)<<endl;
+
+    cout<<(25<<1)<<endl;
+
+    cout<<(25>>2)<<endl;
+
+    cout<<(25<<2)<<endl;
+
+    cout<<(50>>1)<<endl;
+
+    cout<<(50<<1)<<endl;
+
+    cout<<(50>>2)<<endl;
+
+    cout<<(50<<2)<<endl;
+    
+}

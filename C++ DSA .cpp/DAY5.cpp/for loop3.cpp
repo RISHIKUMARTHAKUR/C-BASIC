922f0eaf833196e0c9b00b7212dc707fac9b4226
RISHI  KUMAR  THAKUR <119896306+RISHIKUMARTHAKUR@users.noreply.github.com>
2023-06-08 13:03:21 +0530

    For loop

---
+++ C++ DSA .cpp/DAY5.cpp/for loop3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n;
+    for( int a= 0 , int b = 1, int c = 2; a>= 0 && b>= 0 && c>= 0 ; a++ , b++ , c++  ){
+
+        cout<< a << " "<< b <<" "<< c <<endl;
+
+    }
+}
